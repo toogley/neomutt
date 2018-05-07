305edae6bd0a123f48b1247819cc008dd6b4b37f
Richard Russon <rich@flatcap.org>
2018-05-07 15:33:51 +0100

    empty help mailbox

--- config/magic.h
+++ config/magic.h
@@ -43,6 +43,7 @@ enum MailboxType
   MUTT_NOTMUCH,            ///< 'Notmuch' (virtual) Mailbox type
   MUTT_POP,                ///< 'POP3' Mailbox type
   MUTT_COMPRESSED,         ///< Compressed file Mailbox type
+  MUTT_HELP,               ///< Help/documentation Mailbox type
 };
 
 void magic_init(struct ConfigSet *cs);
